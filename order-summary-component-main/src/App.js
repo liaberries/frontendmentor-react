952ef5b31d363e0f2cfbd07ff0ad69d079b8d56d
Lia Smith <70491947+liaberries@users.noreply.github.com>
2023-07-09 15:42:39 -0700

    updated files

--- order-summary-component-main/src/App.js
+++ order-summary-component-main/src/App.js
@@ -1,5 +1,16 @@
+import { useState } from "react";
+
 export default function App() {
-  return (
-    <div>Hello world</div>
-  )
+  return <section>
+    <div>
+      
+    </div>
+    
+  <div>
+    <h1 className="main-title">Order Summary</h1>
+    <p className="main-paragraph">You can now listen to millions of songs, audiobooks, and podcasts on any 
+    device anywhere you like!
+    </p>
+  </div>
+  </section>;
 }
